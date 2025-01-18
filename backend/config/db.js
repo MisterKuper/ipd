3752188d9348bb8a529e9f8be42efa1310b4e2d1
MisterKuper <102316501+MisterKuper@users.noreply.github.com>
2025-01-18 17:15:20 +0100

    Updated link in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://kolishenkoroman:58810@cluster0.cdsq4.mongodb.net/ipd').then(() => console.log('DB Connected'));
+    await mongoose.connect('mongodb+srv://{username}:{password}@cluster0.cdsq4.mongodb.net/ipd').then(() => console.log('DB Connected'));
 };
